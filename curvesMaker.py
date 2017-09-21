a451cd4f76444ddf2aa67dd63958f604754bfb85
anonymouse <mr.vadim.kravtsov@mail.ru>
2017-09-21 13:01:25 +0300

    Modifiend find_obj

--- curvesMaker.py
+++ curvesMaker.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from numpy import genfromtxt
+import numpy as np
 
 dataDir = '/home/anonymouse/COURSWORK/Stars/3_COURSE/PROJECT/DATA'
 workDir = '/home/anonymouse/COURSWORK/Stars/3_COURSE/PROJECT/WORKDIR'
@@ -23,8 +23,16 @@ def show_database():
 		for stars in dataBase[field]:
 			print '----->' + stars
 
-def find_obj():
-	pass
+def find_obj(objX, objY, field, filt ):
+	global dataBase
+	for st in dataBase[field]:
+		stX, stY = st[:3], st[3:]
+		dist = np.hypot(stX - objX, stY - objY)																														 
+		if dist<4:																		
+			return st
+
+	return False
+
 
 def zeros_appender(val):
 	if val<10:
@@ -38,21 +46,24 @@ def zeros_appender(val):
 def objects_definer(fields):
 	for field in fields.keys():
 		catPath = os.path.join(refDir, field, 'i.cat')
-		objXY = genfromtxt(catPath, usecols = [1,2])
+		objXY = np.genfromtxt(catPath, usecols = [1,2])
 		for xy in objXY:
 			strCoords = zeros_appender(xy[0])+zeros_appender(xy[1])
 			fields[field][strCoords] = {}
-			fields[field][strCoords]['coords'] = [int(xy[0]),int(xy[1])]			
+			#fields[field][strCoords]['coords'] = [int(xy[0]),int(xy[1])]			
 	return fields
 
 def cat_reader(cat, field, filt, date):
 	for line in cat:
 		objX, objY = line[1], line[2]
 		objFlux, objFluxErr = line[5], line[6]
-		find_obj(objX, objY, cat)
+		st = find_obj(objX, objY, field, filt)
+		if fields[field][st][date]: 
+			fields[field][st][date][filt+'Mag'] = 
+
 
 def main():
-	dataBase = objects_definer(fields_definer()) 
+	#dataBase = objects_definer(fields_definer()) 
 	fields = os.listdir(dataDir)
 	listOfFields = [fieldName[:-1] for fieldName in open('list_of_objects.dat', 'r')] #тут бы получше избавляться от переносов строки
 	for field in fields:
@@ -66,9 +77,9 @@ def main():
 					if fileName in ['i.cat','b.cat','v.cat','r.cat']:
 						catPath = os.path.join(pathToCats, fileName)
 						filt = fileName[0]
-						cat = genfromtxt(catPath)
+						cat = np.genfromtxt(catPath)
 						cat_reader(cat, field, filt, date)
 
-
+dataBase = objects_definer(fields_definer()) 
 #main()
 show_database()
