2646fdefbecf4e2149e27038ed63232f5aef7fee
anonymouse <mr.vadim.kravtsov@mail.ru>
2017-09-28 21:08:18 +0300

    plot_curve() work! Now it plot many curves automaticly

--- wwdb.py
+++ wwdb.py
@@ -1,7 +1,10 @@
 import astropy
 import pickle
+import numpy as np
 from astropy import time
-from bokeh.plotting import figure, output_file, show
+import matplotlib.pyplot as plt
+from matplotlib.mlab import stineman_interp
+
 dbFile = open('dataBase.dat', 'rb')
 dataBase = pickle.load(dbFile)
 #for field in dataBase:
@@ -13,9 +16,10 @@ dataBase = pickle.load(dbFile)
 #			for mag in dataBase[field][stars][date]:
 #				print '---------------->'+str(dataBase[field][stars][date])
 #				break
-def separator(field = 's50716', mode = 'map', numbOfObs = 5, treshold = 4):
+def separator(field = 's50716', mode = 'map', numbOfObs = 15, lb = 1, rb = 3):
 	global dataBase
 	bestStars = []
+	outStars = []
 	for star in dataBase[field]:
 		if len(dataBase[field][star])>numbOfObs:
 			bestStars.append(star)
@@ -38,8 +42,9 @@ def separator(field = 's50716', mode = 'map', numbOfObs = 5, treshold = 4):
 					maxMag = max(listOfMag)
 					minMag = min(listOfMag)
 					deltaMag = maxMag - minMag
-					if deltaMag>treshold:
-						print field, star, deltaMag, len(listOfMag)
+					if lb<deltaMag<rb:
+						outStars.append([field, star])
+		return outStars
 
 def plot_map(x, y):
 	output_file = ('result.html')
@@ -49,18 +54,18 @@ def plot_map(x, y):
 	p.line([382,382],[0,255])
 	show(p)
 
-def plot_curve():
+def plot_curve(field, star):
 	dates = []
 	mags = []
-	for date in dataBase['bllac']['054196']:
-		if 'iMag' in dataBase['bllac']['054196'][date]:
+	for date in dataBase[field][star]:
+		if 'iMag' in dataBase[field][star][date]:
 			julianDate = astropy.time.Time(date[:4]+'-'+date[4:6]+'-'+date[6:])
 			fixJDtoMJD = astropy.time.Time('1858-11-17')
 			fixJDtoMJD.format = 'jd'
 			julianDate.format = 'jd' 
 			julianDate = julianDate - fixJDtoMJD
 			dates.append(float(str(julianDate)))
-			mags.append(dataBase['bllac']['054196'][date]['iMag'][0])
+			mags.append(dataBase[field][star][date]['iMag'][0])
 	outList = []
 	for i in range(len(dates)):
 		outList.append([dates[i], mags[i]])
@@ -69,10 +74,23 @@ def plot_curve():
 	for i in range(len(outList)):
 		dates.append(outList[i][0])
 		mags.append(outList[i][1])
-	output_file = ('result.html')
-	p = figure(title="coords of best object", y_range = (18,13), x_axis_label='x', y_axis_label='y')
-	p.circle(dates,mags)
-	show(p)
+	x = dates
+	y = mags
+	xMed = np.median(mags)
+	fig, ax = plt.subplots()
+	ax.plot(x, y, 'o')
+	ax.plot((min(x), max(x)), (xMed, xMed), 'k-', markersize = 1)
+	plt.ylim((max(mags)+1, min(mags)-1))
+	plt.xlim(min(x)-10, max(x)+10)
+	plt.ylabel('Magnitude, m')
+	plt.xlabel('JD')
+	plt.title('Light curve for '+field+'_'+star+'in '+'i')
+	plt.grid(True)
+	plt.savefig('outGraph/'+field+'_'+star+'.svg')
 
-plot_curve()
+bestStars = separator(mode = 'curve')
+print bestStars
+for obj in bestStars:
+	plot_curve(obj[0], obj[1])
+#plot_curve('bllac', '152172')
 #separator(mode='curve')
